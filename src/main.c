67d01197c2ea1a8f7ddd64c08c71f2a7d66b6a23
kshitij9192 <kshitij9192@gmail.com>
2024-09-15 12:23:18 +0530

    Resolve conflicts arising due to rebase

--- src/main.c
+++ src/main.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-    printf("This is a dummy executable!");
+    printf("This is not a dummy executable anymore!");
+
+    int a=0, b=0;
+    scanf("Enter the first integer : %d", &a);
+    scanf("Enter the second integer : %d", &b);
+
+    printf("Result of adding two integers : %d", (a+b));
 
     return 0;
 }
